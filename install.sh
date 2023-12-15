0c8b0565a4fec67f2e7ad9c4e432636eff67065e
TheJuicePapi <134894632+TheJuicePapi@users.noreply.github.com>
2023-12-15 15:33:45 -0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Check if running as root
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run as root"
+    exit
+fi
+
+# Install required packages (adjust based on your needs)
+apt-get update
+apt-get upgrade -y
+
+# Install Python dependencies
+# No dependencies needed
+
+# Create symbolic link for updateme.py
+ln -s "$(pwd)/updateme.py" /usr/local/bin/updateme
+
+echo "Installation complete and shortcut created! You can now run updateme.py from any directory by simply typing 'updateme'."
